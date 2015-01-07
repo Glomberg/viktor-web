f4411bd89081137290074b2eb9f91971da58de0a
Glomberg <Bazz@bk.ru>
2015-01-07 18:15:14 +0300

    123

---
+++ www/grabz/scripts/script.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+	$('.close').click(function(){
+			$('#fixed-enter').css({'display': 'none'});
+	});
+	$('a#enter').click(function(){
+			$('#fixed-enter').css({'display': 'block'});
+	});
+	$('a#reg').click(function(){
+			$('#fixed-reg').css({'display': 'block'});
+	});
+});
